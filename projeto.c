93a2997bd57117f23d502fb7c101e8b834f6091a
Tpotsk <tpotsk@gmail.com>
2025-09-11 14:44:17 -0300

    Add files via upload

--- projeto.c
+++ projeto.c
@@ -3,17 +3,31 @@
 #include <time.h>
 //#include "FILA.h"
 
+void atendePet(Fila *normal, Fila *emergencia){
+    Pet removido;
+    if(FilaVazia(emergencia)){
+        if(FilaVazia(normal)){
+             removido = RemoveDaFila(normal);
+        } else {
+            printf("Não há Pets a serem atendidos!");
+        }
+    } else {
+        Pet removido = RemoveDaFila(emergencia);
+    }
+
+}
+
 int main() {
     int min = 100, max=999;
     srand(time(NULL));
-    //Fila *normal = CriaFila();
-    //Fila *emergencia = CriaFila();
+    //Fila *normal = inicializaFila();
+    //Fila *emergencia = inicializaFila();
     int rnum = min + rand() % (max - min + 1);
     while(1){
         int rnum = min + rand() % (max - min + 1);
         system("pause");
         printf("Bem vindo à clinica veterinária selecione uma opção: \n");
-        printf("(1) - Inserir na Fila de Atendimento\n (2) - Atender"s);
+        printf("(1) - Inserir na Fila de Atendimento\n (2) - Atender");
     }
     return 0;
 }
