274f58bab303e8df57fa46e17ec2c0195f8f70cc
brunoselingardi <brunoselingardi@gmail.com>
2025-09-11 14:46:22 -0300

    print e scan das infos do pet

--- projeto.c
+++ projeto.c
@@ -3,17 +3,45 @@
 #include <time.h>
 //#include "FILA.h"
 
+void inserePet ()
+{
+
+    int ID = min + rand() % (max - min + 1);
+
+    printf("Insira as informações do(a) paciente: \n\n");
+
+    printf("NOME: ");
+    scanf("%[^\n]");
+
+    printf("ESPÉCIE: ");
+    scanf("%[^\n]")
+
+    printf("IDADE: ");
+    scanf("%d");
+
+    printf("DATA DE NASCIMENTO [d/m/a]: ");
+    scanf("%d %d %d")
+
+    printf("PRIORIDADE (0-EMERGENCIA , 1-NORMAL): ")
+    printf("%d")
+
+}
+
 int main() {
     int min = 100, max=999;
     srand(time(NULL));
     //Fila *normal = CriaFila();
     //Fila *emergencia = CriaFila();
     int rnum = min + rand() % (max - min + 1);
-    while(1){
+    /*while(1){
         int rnum = min + rand() % (max - min + 1);
         system("pause");
         printf("Bem vindo à clinica veterinária selecione uma opção: \n");
-        printf("(1) - Inserir na Fila de Atendimento\n (2) - Atender"s);
-    }
+        printf("(1) - Inserir na Fila de Atendimento\n (2) - Atender"s);#
+    }*/
+    inserePet();
     return 0;
 }
+
+
+
